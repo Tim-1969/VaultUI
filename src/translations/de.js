76537ed99d1401ecb7dbd5f54e8758fd037b7139
Laura Hausmann <laura@hausmann.dev>
2021-04-18 13:35:16 +0200

    Update de.js

--- src/translations/de.js
+++ src/translations/de.js
@@ -4,11 +4,15 @@ module.exports = {
   "back_btn": "Zurück",
   "refresh_btn": "Neu laden",
   "me_btn": "Profil/Einstellungen",
+  // These are the page titles
   "me_page_title": "Proil/Einstellungen",
   "home_page_title": "Startseite",
+
+  // These are all o the other translations
   "log_out_btn": "Abmelden",
   "copy_token_btn": "Token kopieren",
   "renew_lease_btn": "Token erneuern",
+
   "vaulturl_text": "Tresor-URL: {{text}}",
   "password_generator_btn": "Passwortgenerator",
   "your_token_expires_in": "Ihr Token läuft in {{date, until_date}} ab"
