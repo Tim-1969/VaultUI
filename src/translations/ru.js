4dcea3c90c10cdb4a15a0cd6c85744402a8616d7
/dev/urandom <dev.urandom@posteo.org>
2021-04-18 14:52:10 +0300

    added russian translation to lines in en.js

---
+++ src/translations/ru.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // These are the buttons on the top bar.
+  "home_btn": "Главная страница",
+  "back_btn": "Назад",
+  "refresh_btn": "Обновить",
+  "me_btn": "Профиль/параметры",
+  // These are the page titles
+  "me_page_title": "Профиль/параметры",
+  "home_page_title": "Главная страница",
+
+  // These are all o the other translations
+  "log_out_btn": "Выход",
+  "copy_token_btn": "Копировать ключ",
+  "renew_lease_btn": "Продлить время действия ключа",
+
+  "vaulturl_text": "Адрес хранилища: {{text}}",
+  "password_generator_btn": "Генератор паролей",
+  "your_token_expires_in": "Ключ годен до: {{date, until_date}}"
+
+}
