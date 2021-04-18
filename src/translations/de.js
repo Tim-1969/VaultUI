70be4945daaf8cdbaee9d20760179f5fbd03b149
Laura Hausmann <laura@hausmann.dev>
2021-04-18 13:31:53 +0200

    Update de.js

--- src/translations/de.js
+++ src/translations/de.js
@@ -2,5 +2,14 @@ module.exports = {
   // These are the buttons on the top bar.
   "home_btn": "Startseite",
   "back_btn": "Zurück",
-  "refresh_btn": "Neu laden"
+  "refresh_btn": "Neu laden",
+  "me_btn": "Profil/Einstellungen",
+  "me_page_title": "Proil/Einstellungen",
+  "home_page_title": "Startseite",
+  "log_out_btn": "Abmelden",
+  "copy_token_btn": "Token kopieren",
+  "renew_lease_btn": "Token erneuern",
+  "vaulturl_text": "Tresor-URL: {{text}}",
+  "password_generator_btn": "Passwortgenerator",
+  "your_token_expires_in": "Ihr Token läuft in {{date, until_date}} ab"
 }
