17625f408d540e47afc881478e8ddbd30118c2f8
Kitteh <kitteh@kitteh.pw>
2021-05-03 15:56:21 +0100

    Add some more options to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,23 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const VERBOSE = Object.getOwnPropertyNames(process.env).includes("VERBOSE") || !true;
+const MODE  = process.env.WEBPACK_MODE || "production" 
+const DEBUG = MODE != "production";
+
+
 module.exports = {
-  mode: process.env.WEBPACK_MODE || "production",
+  mode: MODE,
+  cache: DEBUG,
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  stats: {
+    preset: VERBOSE ? "detailed" : "normal",
+    colors: true,
+    timings: true,
   },
   plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin({ title: "VaultUI" })],
   devServer: {
