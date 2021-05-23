7672eaa32a5f3febe58ef1c0cb2fff70a1097423
Kitteh <kitteh@kitteh.pw>
2021-05-23 13:46:35 +0100

    Remove some elements that are no longer in use.

--- src/elements/ListItem.ts
+++
@@ -1,8 +0,0 @@
-import { makeElement } from "z-makeelement";
-
-export function ListItem(children: Element[] | Element): HTMLElement {
-  return makeElement({
-    tag: "li",
-    children: children,
-  });
-}
