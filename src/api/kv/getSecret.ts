77105dca45ae3aceffadec3805f15bb58eeae712
Kitteh <kitteh@kitteh.pw>
2021-05-28 09:50:07 +0100

    Seporate getSecret into v1 and v2.

--- src/api/kv/getSecret.ts
+++ src/api/kv/getSecret.ts
@@ -1,19 +1,33 @@
 import { appendAPIURL, checkResponse, getHeaders } from "../apiUtils";
 
-export async function getSecret(
+export async function getSecretKV1(
   baseMount: string,
-  secretMountType: string,
+  secretPath: string[],
+  name: string,
+): Promise<Record<string, unknown>> {
+  const request = new Request(appendAPIURL(`/v1/${baseMount}/${secretPath.join("")}/${name}`), {
+    headers: getHeaders(),
+  });
+
+  const resp = await fetch(request);
+  await checkResponse(resp);
+
+  const data = (await resp.json()) as unknown;
+
+  return (data as { data: Record<string, unknown> }).data;
+}
+
+export async function getSecretKV2(
+  baseMount: string,
   secretPath: string[],
   name: string,
   version: string | null = null,
 ): Promise<Record<string, unknown>> {
   let secretURL = "";
-  if (secretMountType == "kv-v2") {
-    secretURL = `/v1/${baseMount}/data/${secretPath.join("")}/${name}`;
-    if (version != null) secretURL += `?version=${version}`;
-  } else {
-    secretURL = `/v1/${baseMount}/${secretPath.join("")}/${name}`;
-  }
+
+  secretURL = `/v1/${baseMount}/data/${secretPath.join("")}/${name}`;
+  if (version != null) secretURL += `?version=${version}`;
+
   const request = new Request(appendAPIURL(secretURL), {
     headers: getHeaders(),
   });
@@ -21,11 +35,20 @@ export async function getSecret(
   const resp = await fetch(request);
   await checkResponse(resp);
 
-
   const data = (await resp.json()) as unknown;
+  return (data as { data: { data: Record<string, unknown> } }).data.data;
+}
+
+export async function getSecret(
+  baseMount: string,
+  secretMountType: string,
+  secretPath: string[],
+  name: string,
+  version: string | null = null,
+): Promise<Record<string, unknown>> {
   if (secretMountType == "kv-v2") {
-    return (data as { data: { data: Record<string, unknown> } }).data.data;
+    return await getSecretKV2(baseMount, secretPath, name, version)
   } else {
-    return (data as { data: Record<string, unknown> }).data;
+    return await getSecretKV1(baseMount, secretPath, name)
   }
 }
